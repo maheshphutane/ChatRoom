b9fd23c854a35360db1bc15e57abc269a8ad52d1
Mahesh phutane <43848331+maheshphutane@users.noreply.github.com>
2020-07-31 10:34:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ io.on('connection', socket => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatRoom!'));
 
     // Broadcast when a user connects
     socket.broadcast
